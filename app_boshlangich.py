91ffabe6baee1842c9cc9d6560aba76971e3c12d
baxtiyornajimov871-byte <baxtiyornajimov871@gmail.com>
2026-04-26 01:23:27 +0500

    Update app_boshlangich.py

--- app_boshlangich.py
+++ app_boshlangich.py
@@ -366,6 +366,7 @@ CHAT_HTML = r"""<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8"/>
+<meta name="google-site-verification" content="quvbrODXfwsubyDVXs2BoOywLJYIIMRdRR8wuZAxoXY" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>Baxtiyor AI</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
